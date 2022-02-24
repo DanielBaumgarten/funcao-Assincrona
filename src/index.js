c0db23a53e21e903c9d6ef7593d4499ab85bd061
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-02-24 08:13:09 -0300

    exercico aula funcoes assinscronas

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const {getCityFromZipcode, getPackageDescriptionNpm} = require('utils-playground');
+
+const app = express();
+
+app.get('/' , async (req,res) => {
+    const cidade =  getCityFromZipcode('90150002');
+    const cidade2 =  getCityFromZipcode('41256250');
+
+    const promise = await Promise.all ([cidade, cidade2]);
+
+    const [resposta, resposta2] = promise;
+
+    res.send(`a cidade encontrado foi: ${resposta} e ${resposta2}.`)
+});
+
+app.get('/pacote/:nomePacote', async (req,res) => {
+    const { nomePacote } = req.params;
+
+    const descricaoPacote = await getPackageDescriptionNpm(nomePacote);
+
+    return res.send(descricaoPacote);
+});
+
+
+app.listen(3000);
+
